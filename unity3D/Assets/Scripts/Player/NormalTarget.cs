758c75b849aa907fa0bfa8542bd6ef0f5a51d783
jaycoap <70280331+jaycoap@users.noreply.github.com>
2022-03-30 23:20:58 +0900

    2022-03-30 PM23:19
    Player에 기본공격 범위인 NormalTarget, 스킬 공격범위인 SkillTarget 추가
    NormalTarget.cs: 기본공격 범위에 들어오는 적 들설정.

---
+++ unity3D/Assets/Scripts/Player/NormalTarget.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalTarget : MonoBehaviour
+{
+    // 일반 공격을 할 때 공격 반경에 있는 적들의 리스트를 관리하는 클래스
+
+    public List<Collider> targetList;// 공격 대상에 있는 적들의 리스트
+
+    void Awake()
+    {
+        targetList = new List<Collider>(); // 오브젝트가 생성될 때 호출되는 Awake에서 targetList 배열을 초기화
+    }
+
+    private void OnTriggerEnter(Collider other) // 적 개체가 공격 반경 안에 들어오면, targetList에 해당 개체를 추가한다.
+    {
+        targetList.Add(other);
+    }
+
+    private void OnTriggerExit(Collider other) // 적 개체가 공격 반경에서 벗어나면 targetList에서 삭제한다.
+    {
+        targetList.Remove(other);  
+    }
+
+
+}
